84f023f292a6f5b72bdede1a107a6ef3d4f8da6e
Joe Malt <joemalt1832@yahoo.co.uk>
2012-07-09 19:53:44 +0100

    Add .gitignore

--- dbconn.php
+++ dbconn.php
@@ -1,7 +1,7 @@
 <?php
 define("DB_HOST","localhost");
 define("DB_USER","root"); //Root is bad, change this sometime
-define("DB_PASSWORD","rootMySQL153");
+define("DB_PASSWORD","somerootpassword");
 define("DB_NAME","yzReact");
 
 function connect()
